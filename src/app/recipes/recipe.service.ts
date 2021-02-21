4b0e8ec202739e707b0818b6e6f0a9c91b3783bd
Salim Salimi <salim.salimi@protonmail.com>
2021-02-21 23:12:19 +0100

    get selected recipe using RecipeService

--- src/app/recipes/recipe.service.ts
+++ src/app/recipes/recipe.service.ts
@@ -1,6 +1,10 @@
+import { EventEmitter } from "@angular/core";
+
 import {Recipe} from "./recipe.model";
 
 export class RecipeService {
+  recipeSelected = new EventEmitter<Recipe>();
+
   private recipes: Recipe[] = [
     new Recipe("A Test Recipe", "This is a test", "https://centslessdeals.com/wp-content/uploads/2018/06/Spinach-Quiche-Recipe-4.jpg"),
     new Recipe("Another Test Recipe", "This is a second test", "https://centslessdeals.com/wp-content/uploads/2018/06/Spinach-Quiche-Recipe-4.jpg")
