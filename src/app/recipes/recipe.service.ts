47364e37327f94be674b630be5c1d797570df962
Salim Salimi <salim.salimi@protonmail.com>
2021-02-22 00:40:22 +0100

    add ingredients to recipe and show list of ingredients in recipe-detail

--- src/app/recipes/recipe.service.ts
+++ src/app/recipes/recipe.service.ts
@@ -1,13 +1,22 @@
-import { EventEmitter } from "@angular/core";
+import {EventEmitter} from "@angular/core";
 
 import {Recipe} from "./recipe.model";
+import {Ingredient} from "../shared/ingredient.model";
 
 export class RecipeService {
   recipeSelected = new EventEmitter<Recipe>();
 
   private recipes: Recipe[] = [
-    new Recipe("A Test Recipe", "This is a test", "https://centslessdeals.com/wp-content/uploads/2018/06/Spinach-Quiche-Recipe-4.jpg"),
-    new Recipe("Another Test Recipe", "This is a second test", "https://centslessdeals.com/wp-content/uploads/2018/06/Spinach-Quiche-Recipe-4.jpg")
+    new Recipe(
+      "A Test Recipe",
+      "This is a test",
+      "https://centslessdeals.com/wp-content/uploads/2018/06/Spinach-Quiche-Recipe-4.jpg",
+      [new Ingredient("Tomatoes", 15), new Ingredient("Apples", 12)]),
+    new Recipe(
+      "Another Test Recipe",
+      "This is a second test",
+      "https://centslessdeals.com/wp-content/uploads/2018/06/Spinach-Quiche-Recipe-4.jpg",
+      [new Ingredient("Oranges", 1), new Ingredient("Bananas", 5)])
   ];
 
   getRecipes() {
