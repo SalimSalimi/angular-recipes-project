745a8313ddfe8ef4cc0dee61aa5cf119a191f910
Salim Salimi <salim.salimi@protonmail.com>
2021-02-22 22:19:24 +0100

    fix reload links and /recipes route

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,11 @@
 import {NgModule} from "@angular/core";
 import {RouterModule, Routes} from "@angular/router";
-import {RecipeListComponent} from "./recipes/recipe-list/recipe-list.component";
 import {ShoppingListComponent} from "./shopping-list/shopping-list.component";
+import {RecipesComponent} from "./recipes/recipes.component";
 
 const appRoutes: Routes = [
   { path: '', redirectTo: '/recipes', pathMatch: 'full' },
-  { path: 'recipes', component: RecipeListComponent},
+  { path: 'recipes', component: RecipesComponent},
   { path: 'shopping-list', component: ShoppingListComponent }
 ]
 
