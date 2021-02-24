89fc865cff4ca93c27110e414ab2b42348427e32
Salim Salimi <salim.salimi@protonmail.com>
2021-02-24 14:00:46 +0100

    Add button add ingredient to add ingredient

--- src/app/recipes/recipe-edit/recipe-edit.component.ts
+++ src/app/recipes/recipe-edit/recipe-edit.component.ts
@@ -63,4 +63,13 @@ export class RecipeEditComponent implements OnInit {
   onSubmit() {
 
   }
+
+  onAddIngredient() {
+    (<FormArray>this.recipeForm.get('ingredients')).push(
+      new FormGroup({
+        'name': new FormControl(),
+        'amount': new FormControl()
+      })
+    )
+  }
 }
