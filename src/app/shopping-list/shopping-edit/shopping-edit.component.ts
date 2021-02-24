01e08f586c1e48c702519873a589f1c2f64dc813
Salim Salimi <salim.salimi@protonmail.com>
2021-02-24 13:23:49 +0100

    Delete an ingredient

--- src/app/shopping-list/shopping-edit/shopping-edit.component.ts
+++ src/app/shopping-list/shopping-edit/shopping-edit.component.ts
@@ -50,6 +50,11 @@ export class ShoppingEditComponent implements OnInit, OnDestroy {
     this.shoppingEditForm.reset();
   }
 
+  onDelete() {
+    this.shoppingListService.deleteIngredient(this.editedItemIndex);
+    this.onClear();
+  }
+
   ngOnDestroy() {
     this.subscription.unsubscribe();
   }
