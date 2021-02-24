a806336c4ab99e980afd9c2b6f685559b1bd8f2c
Salim Salimi <salim.salimi@protonmail.com>
2021-02-24 12:56:48 +0100

    store and send index using subjects of selected ingredients to edit

--- src/app/shopping-list/shopping-edit/shopping-edit.component.ts
+++ src/app/shopping-list/shopping-edit/shopping-edit.component.ts
@@ -1,18 +1,26 @@
-import {Component, OnInit} from '@angular/core';
+import {Component, OnDestroy, OnInit} from '@angular/core';
 import {Ingredient} from "../../shared/ingredient.model";
 import {ShoppingListService} from "../shopping-list.service";
 import {NgForm} from "@angular/forms";
+import {Subscription} from "rxjs";
 
 @Component({
   selector: 'app-shopping-edit',
   templateUrl: './shopping-edit.component.html',
   styleUrls: ['./shopping-edit.component.css']
 })
-export class ShoppingEditComponent implements OnInit {
+export class ShoppingEditComponent implements OnInit, OnDestroy {
+  private subscription!: Subscription;
+  editMode = false;
+  editedItemIndex!: number;
 
   constructor(private shoppingListService: ShoppingListService) { }
 
   ngOnInit(): void {
+    this.subscription = this.shoppingListService.startedEditing.subscribe((index: number) => {
+      this.editMode = true;
+      this.editedItemIndex = index;
+    })
   }
 
   onAddItem(form: NgForm) {
@@ -22,5 +30,9 @@ export class ShoppingEditComponent implements OnInit {
       value.amount
     );
     this.shoppingListService.addIngredient(ingredient);
+  }
+
+  ngOnDestroy() {
+    this.subscription.unsubscribe();
   }
 }
