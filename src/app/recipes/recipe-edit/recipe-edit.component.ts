29aaeca74e92d44bb162888c0896fbb5841f89b9
Salim Salimi <salim.salimi@protonmail.com>
2021-02-24 22:53:02 +0100

    Delete a recipe using delete button

--- src/app/recipes/recipe-edit/recipe-edit.component.ts
+++ src/app/recipes/recipe-edit/recipe-edit.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import {ActivatedRoute, Params} from "@angular/router";
+import {ActivatedRoute, Params, Router} from "@angular/router";
 import {FormArray, FormControl, FormGroup, Validators} from "@angular/forms";
 import {RecipeService} from "../recipe.service";
 import {Recipe} from "../recipe.model";
@@ -16,7 +16,8 @@ export class RecipeEditComponent implements OnInit {
 
   constructor(
     private route: ActivatedRoute,
-    private recipeService: RecipeService) { }
+    private recipeService: RecipeService,
+    private router: Router) { }
 
   ngOnInit(): void {
     this.route.params.subscribe((params: Params) => {
@@ -61,6 +62,10 @@ export class RecipeEditComponent implements OnInit {
     return (<FormArray>this.recipeForm.get('ingredients')).controls;
   }
 
+  navigateAway() {
+    this.router.navigate(['../'], {relativeTo: this.route});
+  }
+
   onSubmit() {
     const recipe = new Recipe(
       this.recipeForm.value['name'],
@@ -72,6 +77,7 @@ export class RecipeEditComponent implements OnInit {
       this.recipeService.updateRecipe(this.id, recipe)
     else
       this.recipeService.addRecipe(this.recipeForm.value);
+    this.navigateAway();
   }
 
   onAddIngredient() {
@@ -81,5 +87,9 @@ export class RecipeEditComponent implements OnInit {
         'amount': new FormControl(null, [Validators.required, Validators.pattern(/^[1-9]+[0-9]*$/)])
       })
     )
+  }
+
+  onCancel() {
+    this.navigateAway();
   }
 }
