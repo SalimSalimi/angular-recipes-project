5f267bcf26810d08ee3343305983416597c0229b
Salim Salimi <salim.salimi@protonmail.com>
2021-02-25 21:58:31 +0100

    sending data to firebase

--- src/app/shared/data-storage.service.ts
+++ src/app/shared/data-storage.service.ts
@@ -1,11 +1,27 @@
 import {Injectable} from '@angular/core';
 import {HttpClient} from "@angular/common/http";
+import {Recipe} from "../recipes/recipe.model";
+import {RecipeService} from "../recipes/recipe.service";
 
 @Injectable({
   providedIn: 'root'
 })
 export class DataStorageService {
 
-  constructor(private http: HttpClient) {
+  private BASE_URL = 'https://angular-recipes-project-course-default-rtdb.firebaseio.com/';
+  private RECIPES_URL = this.BASE_URL + 'recipes.json';
+
+  constructor(
+    private http: HttpClient,
+    private recipeService: RecipeService) {}
+
+  storeRecipes() {
+    const recipes = this.recipeService.getRecipes();
+    this.http.put(
+      this.RECIPES_URL,
+      recipes
+    ).subscribe((response) => {
+      console.log(response);
+    });
   }
 }
