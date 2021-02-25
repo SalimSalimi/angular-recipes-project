f4c10da89ca5e6051d125ce88592e8e3a5bca8e6
Salim Salimi <salim.salimi@protonmail.com>
2021-02-25 22:10:39 +0100

    transforming response data

--- src/app/shared/data-storage.service.ts
+++ src/app/shared/data-storage.service.ts
@@ -2,6 +2,7 @@ import {Injectable} from '@angular/core';
 import {HttpClient} from "@angular/common/http";
 import {Recipe} from "../recipes/recipe.model";
 import {RecipeService} from "../recipes/recipe.service";
+import {map} from "rxjs/operators";
 
 @Injectable({
   providedIn: 'root'
@@ -13,7 +14,8 @@ export class DataStorageService {
 
   constructor(
     private http: HttpClient,
-    private recipeService: RecipeService) {}
+    private recipeService: RecipeService) {
+  }
 
   storeRecipes() {
     const recipes = this.recipeService.getRecipes();
@@ -27,6 +29,15 @@ export class DataStorageService {
 
   fetchRecipes() {
     this.http.get<Recipe[]>(this.RECIPES_URL)
+      .pipe(map(recipes => {
+        return recipes.map(recipe => {
+          return {
+            ...recipe,
+            ingredients: recipe.ingredients ?
+              recipe.ingredients : []
+          }
+        });
+      }))
       .subscribe((recipes) => {
         this.recipeService.setRecipes(recipes);
       })
