f30f3aa8427b47fab1aaf0c1c5b5c04d9821c3aa
Salim Salimi <salim.salimi@protonmail.com>
2021-02-26 15:09:47 +0100

    Implementing login functionnality

--- src/app/auth/auth/auth.service.ts
+++ src/app/auth/auth/auth.service.ts
@@ -3,22 +3,24 @@ import {HttpClient} from "@angular/common/http";
 import {catchError} from "rxjs/operators";
 import {throwError} from "rxjs";
 
-interface AuthResponseData {
+export interface AuthResponseData {
   kind: string;
   idToken: string;
   email: string;
   refreshToken: string;
   expiresIn: string;
   localId: string;
+  registered?: boolean
 }
 
 @Injectable({
   providedIn: 'root'
 })
 export class AuthService {
-  private BASE_AUTH = 'https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=';
+  private BASE_AUTH = 'https://identitytoolkit.googleapis.com/v1/accounts';
   private API_KEY = 'AIzaSyA7sbkPEbumREa2aEWTQy2fpBkC2Yw2lDc';
-  private SIGN_AUTH_ENDPOINT = this.BASE_AUTH + this.API_KEY;
+  private SIGN_UP_AUTH_ENDPOINT = this.BASE_AUTH + ':signUp?key=' + this.API_KEY;
+  private SIGN_IN_AUTH_ENDPOINT = this.BASE_AUTH + ':signInWithPassword?key=' + this.API_KEY;
 
   constructor(
     private http: HttpClient
@@ -27,7 +29,7 @@ export class AuthService {
 
   signUp(email: string, password: string) {
     return this.http.post<AuthResponseData>(
-      this.SIGN_AUTH_ENDPOINT,
+      this.SIGN_UP_AUTH_ENDPOINT,
       {
         email: email,
         password: password,
@@ -44,5 +46,16 @@ export class AuthService {
       }
       return throwError(errorMessage);
     }))
+  }
+
+  logIn(email: string, password: string) {
+    return this.http.post<AuthResponseData>(
+      this.SIGN_IN_AUTH_ENDPOINT,
+      {
+        email: email,
+        password: password,
+        returnSecureToken: true
+      }
+    )
   }
 }
