ab532e70e7997031a45d0ee717bfd4d5704ee5c4
Salim Salimi <salim.salimi@protonmail.com>
2021-02-28 14:23:35 +0100

    lazy loading recipes module

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,19 @@
 import {NgModule} from "@angular/core";
 import {RouterModule, Routes} from "@angular/router";
-import {AuthComponent} from "./auth/auth.component";
 
 const appRoutes: Routes = [
   {
     path: '', redirectTo: '/recipes', pathMatch: 'full'
+  },
+  {
+    // Loading recipes module/components/routes when visiting 'recipes'
+    path: 'recipes',
+    //loadChildren: './recipes/recipes.module#RecipesModule'
+    // more modern import
+    loadChildren: () =>
+      import('./recipes/recipes.module').then(m =>
+        m.RecipesModule)
+
   }
 ]
 
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,9 +4,7 @@ import {BrowserModule} from '@angular/platform-browser';
 import {AppComponent} from './app.component';
 import {HeaderComponent} from './header/header-component';
 import {AppRoutingModule} from "./app-routing.module";
-import {FormsModule, ReactiveFormsModule} from "@angular/forms";
 import {HttpClientModule} from "@angular/common/http";
-import {RecipesModule} from "./recipes/recipes.module";
 import {ShoppingListModule} from "./shopping-list/shopping-list.module";
 import {SharedModule} from "./shared/shared.module";
 import {CoreModule} from "./core.module";
@@ -22,7 +20,6 @@ import {AuthModule} from "./auth/auth.module";
     HttpClientModule,
     AppRoutingModule,
     AuthModule,
-    RecipesModule,
     ShoppingListModule,
     SharedModule,
     CoreModule
